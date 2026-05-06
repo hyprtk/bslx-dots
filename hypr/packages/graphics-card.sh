c2eaf650337d7935ed8c1bc1fa750794ac1491b2
Kori Tk <66495933+hyprtk@users.noreply.github.com>
2026-05-06 13:37:01 +0100

    Update graphics-card.sh

--- hypr/packages/graphics-card.sh
+++ hypr/packages/graphics-card.sh
@@ -17,8 +17,9 @@ echo ""
 read GRAPHICSCARD
 case $GRAPHICSCARD in
 1)
-  sudo pacman -S --noconfirm xf86-video-intel mesa vulkan-intel vulkan-intel;;
+  sudo pacman -S --noconfirm xf86-video-intel mesa vulkan-intel;;
 2)
+  sudo pacman -Rns --noconfirm nvidia-open-dkms nvidia-utils nvidia-settings qt5-wayland qt5ct qt6-wayland qt6ct libva && yay --noconfirm -Rns libva-nvidia-driver-git
   sudo pacman -S --noconfirm xf86-video-amdgpu mesa vulkan-radeon vdpauinfo corectrl libvdpau vdpauinfo
   sudo sed -i 's/MODULES=()/MODULES=(amdgpu)/' /etc/mkinitcpio.conf
   sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img;;
